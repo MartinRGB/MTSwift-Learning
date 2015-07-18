fa1bceb706517378df70e0f93a8f3560e3a357af
MartinRGB <qiuyinsen@gmail.com>
2015-07-18 22:31:20 +0800

    Put all tutorial practice into one demo
    Put all tutorial practice into one demo

--- 5.MathewSanders.com/Container ViewTransition/Container ViewTransition/ViewController.swift
+++ 5.MathewSanders.com/Container ViewTransition/Container ViewTransition/ViewController.swift
@@ -10,29 +10,75 @@ import UIKit
 
 class ViewController: UIViewController{
 
-    let Effectname = ["Effect 1","Effect 2","Effect 3","Effect 4","Effect 5"]
+    let Effectname = ["Animation 1","Animation 2","Transition 1","Transition 2","Transition 3"]
     
     let textCellIdentifier = "TextCell"
     
-    //每栏行数
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    //在本例中，只有一个分区
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
         return 1
     }
     
-    //多少栏
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //多少行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return Effectname.count
     }
     
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    //创建各单元显示内容(创建参数indexPath指定的单元）
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        //同一形式的单元格重复使用
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-        
+        //每一行的名字
         let row = indexPath.row
         cell.textLabel?.text = Effectname[row]
         
         return cell
     }
+    
+    // UITableViewDelegate 方法，处理列表项的选中事件
+    func tableView(tableView:UITableView!,didSelectRowAtIndexPath indexPath:NSIndexPath!)
+    {
+        println("You selected cell #\(indexPath.row)!")
+        if indexPath.row == 0{
+            self.performSegueWithIdentifier("effect1", sender: indexPath.row)
+        }
+        if indexPath.row == 1{
+            self.performSegueWithIdentifier("effect2", sender: indexPath.row)
+        }
+        if indexPath.row == 2{
+            self.performSegueWithIdentifier("effect3", sender: indexPath.row)
+        }
+        if indexPath.row == 3{
+            self.performSegueWithIdentifier("effect4", sender: indexPath.row)
+        }
+        if indexPath.row == 4{
+            self.performSegueWithIdentifier("effect5", sender: indexPath.row)
+        }
+        
+    }
+    
+    //开启转场
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "effect1"{
+            let controller = segue.destinationViewController as! animation1
+        }
+        if segue.identifier == "effect2"{
+            let controller = segue.destinationViewController as! animation2
+        }
+        if segue.identifier == "effect3"{
+            let controller = segue.destinationViewController as! transition1
+        }
+        if segue.identifier == "effect4"{
+            let controller = segue.destinationViewController as! transition2
+        }
+        if segue.identifier == "effect5"{
+            let controller = segue.destinationViewController as! transition3
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ 5.MathewSanders.com/Container ViewTransition/Container ViewTransition/animation1.swift
@@ -0,0 +1,76 @@
+
+//
+//  zeroViewController.swift
+//  Container ViewTransition
+//
+//  Created by Martin on 15/7/18.
+//  Copyright (c) 2015年 1. All rights reserved.
+//
+
+import UIKit
+
+class animation1: UIViewController {
+    
+    @IBOutlet weak var UIValueSlider: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func ButtonPressed(sender: AnyObject) {
+        //set slider value into loop
+        let numofdoge = Int(self.UIValueSlider.value)
+        
+        //set 10 icon
+        for loopNumber in 0...numofdoge{
+            //CGFloat (arc4random_uniform())
+            
+            let size : CGFloat = CGFloat (arc4random_uniform(40)) + 20
+            let yPosition : CGFloat = CGFloat (arc4random_uniform(200))+110
+            
+            /*let color = UIColor(red:CGFloat (arc4random_uniform(255))/255.0,green:CGFloat (arc4random_uniform(255))/255.0,blue:CGFloat (arc4random_uniform(255))/255.0,alpha:1.0)*/
+            
+            let colorcube = UIImageView()
+            //0-size for out of screen
+            colorcube.frame = CGRectMake(0-size, yPosition, size, size)
+            colorcube.image = UIImage(named:"Doge.png")
+            self.view.addSubview(colorcube)
+            
+            let duration : NSTimeInterval = 1.0
+            let delay: NSTimeInterval = NSTimeInterval(100 + arc4random_uniform(900))/200
+            let options = UIViewAnimationOptions.CurveLinear
+            
+            
+            
+            UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                
+                /*colorcube.backgroundColor = UIColor(red:CGFloat (arc4random_uniform(255))/255.0,green:CGFloat (arc4random_uniform(255))/255.0,blue:CGFloat (arc4random_uniform(255))/255.0,alpha:1.0)*/
+                colorcube.frame = CGRectMake(320, yPosition, size, size)
+                
+                }, completion: { animationFinished in
+                    colorcube.removeFromSuperview()
+                    
+            })
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
