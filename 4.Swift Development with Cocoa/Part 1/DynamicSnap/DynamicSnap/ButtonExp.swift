db78f1ee5822554426b96a249ea387a5b5d4b70b
MartinRGB <qiuyinsen@gmail.com>
2015-07-22 12:44:48 +0800

    add a red oval stroke
    add a red oval stroke

--- 4.Swift Development with Cocoa/Part 1/DynamicSnap/DynamicSnap/ButtonExp.swift
+++ 4.Swift Development with Cocoa/Part 1/DynamicSnap/DynamicSnap/ButtonExp.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ButtonExp: UIViewController {
     
+    @IBOutlet weak var dribbbleview: UIImageView!
+    
     var dynamicAnimator:UIDynamicAnimator?
     var nr1: UIDynamicItemBehavior!
     var nr2: UIDynamicItemBehavior!
@@ -25,6 +27,7 @@ class ButtonExp: UIViewController {
     let btn2 = UIImageView()
     let btn3 = UIImageView()
     let btn4 = UIImageView()
+    var center = UIImageView()
     
    
     
@@ -33,22 +36,27 @@ class ButtonExp: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //set the UIImage&Imageview
         btn1.image = UIImage(named: "Btn1.png")
         btn2.image = UIImage(named: "Btn2.png")
         btn3.image = UIImage(named: "Btn3.png")
         btn4.image = UIImage(named: "Btn4.png")
+        center.image = UIImage(named:"Center.png")
         
         btn1.alpha = 0
         btn2.alpha = 0
         btn3.alpha = 0
         btn4.alpha = 0
+        center.alpha = 0
+        center.transform = CGAffineTransformMakeScale(2, 2)
         
         
         self.view.addSubview(btn1)
         self.view.addSubview(btn2)
         self.view.addSubview(btn3)
         self.view.addSubview(btn4)
-        
+        self.view.addSubview(center)
+        //add DynamicAnimator
         self.dynamicAnimator = UIDynamicAnimator(referenceView: self.view);
         
         
@@ -62,18 +70,27 @@ class ButtonExp: UIViewController {
     
 
     @IBAction func longpressed(sender: AnyObject) {
+        //set 4 popup point
         let lpress = sender as! UILongPressGestureRecognizer
         var point = lpress.locationInView(self.view)
-        var point1 = CGPointMake(point.x-126, point.y-70)
-        var point2 = CGPointMake(point.x-66, point.y-121)
-        var point3 = CGPointMake(point.x+22, point.y-121)
-        var point4 = CGPointMake(point.x+87, point.y-70)
+        var point1 = CGPointMake(point.x-77, point.y-17)
+        var point2 = CGPointMake(point.x-39, point.y-72)
+        var point3 = CGPointMake(point.x+24, point.y-72)
+        var point4 = CGPointMake(point.x+67, point.y-17)
         
-    
+        center.frame = CGRectMake(point.x-28, point.y-28, 54, 56)
+        println(point)
         
+        
+        UIView.animateWithDuration(0.15, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            
+            self.center.alpha = 1
+            }, completion: {finished in})
+
+
         if sender.state == UIGestureRecognizerState.Began{
-            
-            
+    
+            //no rotation make effect smooth
             nr1 = UIDynamicItemBehavior(items: [self.btn1])
             nr1.allowsRotation = false
             nr2 = UIDynamicItemBehavior(items: [self.btn2])
@@ -88,22 +105,17 @@ class ButtonExp: UIViewController {
             btn3.frame = CGRectMake(point.x, point.y, 48, 48)
             btn4.frame = CGRectMake(point.x, point.y, 48, 48)
             
+            //alpha fade in
             UIView.animateWithDuration(0.15, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
                 self.btn1.alpha = 1
                 self.btn2.alpha = 1
                 self.btn3.alpha = 1
                 self.btn4.alpha = 1
+                self.center.alpha = 1
             }, completion: {finished in})
            
             self.dynamicAnimator?.removeAllBehaviors()
-            
-            UIView.animateWithDuration(0.15, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
-                self.btn1.alpha = 1
-                self.btn2.alpha = 1
-                self.btn3.alpha = 1
-                self.btn4.alpha = 1
-                }, completion: {finished in})
-            
+            //snap effect
             self.snap1 = UISnapBehavior(item:self.btn1 ,snapToPoint:point1)
             self.snap2 = UISnapBehavior(item:self.btn2 ,snapToPoint:point2)
             self.snap3 = UISnapBehavior(item:self.btn3 ,snapToPoint:point3)
@@ -117,26 +129,28 @@ class ButtonExp: UIViewController {
             self.dynamicAnimator?.addBehavior(self.snap4)
             
         }
+
          if sender.state == UIGestureRecognizerState.Ended{
             
             
             
             
-            
+            //alpha fade out
             UIView.animateWithDuration(0.25, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
                 self.btn1.alpha = 0
                 self.btn2.alpha = 0
                 self.btn3.alpha = 0
                 self.btn4.alpha = 0
+                self.center.alpha = 0
                 }, completion: {finished in})
             
             self.dynamicAnimator?.removeAllBehaviors()
-            
+            // no rotation make smooth
             self.dynamicAnimator?.addBehavior(nr1)
             self.dynamicAnimator?.addBehavior(nr2)
             self.dynamicAnimator?.addBehavior(nr3)
             self.dynamicAnimator?.addBehavior(nr4)
-            
+            //snap back
             self.snap1 = UISnapBehavior(item:self.btn1 ,snapToPoint:point)
             self.snap2 = UISnapBehavior(item:self.btn2 ,snapToPoint:point)
             self.snap3 = UISnapBehavior(item:self.btn3 ,snapToPoint:point)
